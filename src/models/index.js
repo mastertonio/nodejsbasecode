4c5006213b94920667b967a2af2625e12fd6759f
anthonyderecho <anthony.derecho.ns@gmail.com>
2022-05-17 19:04:56 +0800

    Company Model

--- src/models/index.js
+++ src/models/index.js
@@ -2,3 +2,4 @@ module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Calculator = require('./calculators.model');
 module.exports.Cell = require('./cell.model');
+module.exports.Company = require('./company.model');
