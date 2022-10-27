962bd75c9f5d999944c883c0400c0f94acfc200d
roishopAdmin <amin@theroishop.com>
2022-10-27 20:00:30 +0800

    update the auth

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ const { NO_CONTENT } = require('http-status');
   var corsOptionsDelegate = function (req, callback) {
     var corsOptions;
     if (allowlist.indexOf(req.header('Origin')) !== -1) {
-      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+      corsOptions = { origin: true ,credentials: true} // reflect (enable) the requested origin in the CORS response
     } else {
       corsOptions = { origin: false } // disable CORS for this request
     }
