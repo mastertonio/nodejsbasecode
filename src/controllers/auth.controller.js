fae32aa79644569bd1a6f9e8892e2bfe9034936f
roishopAdmin <amin@theroishop.com>
2022-10-27 19:29:58 +0800

    askdfjs

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -15,7 +15,7 @@ const login = catchAsync(async (req, res) => {
   const tokens = await tokenService.generateAuthTokens(user, req );
   res.cookie('x-access-token', 
     tokens.access.token,
-    { maxAge: 1000 * 60 * 10, httpOnly: true, sameSite: 'none', secure: true }
+    { maxAge: 1000 * 60 * 10, httpOnly: true, sameSite: 'none', secure: false }
     ); 
   res.send({ user, tokens });
 });
