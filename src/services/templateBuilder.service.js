c6667692e92c2172d8ee5ed72d564e41f8d91a86
hydra54a <anthony.derecho.ns@gmail.com>
2023-04-21 13:47:51 +0800

    patch for template builder

--- src/services/templateBuilder.service.js
+++ src/services/templateBuilder.service.js
@@ -12,6 +12,7 @@ const { map } = require('underscore');
 const { objectId } = require('../validations/custom.validation');
 const { LOGGER_INVALID_TOKEN, INSUFFICIENT_LICENSE, LICENSE_ERROR, ROLE, USER_ERROR, NO_RECORD_FOUND } = require('../common/staticValue.common');
 const sectionBuilder = require('../models/sectionBuilder.model');
+const { templateBuilderService } = require('.');
 
 const  createAdminTool = async(req)=>{
     try {
@@ -24,10 +25,33 @@ const  createAdminTool = async(req)=>{
         throw e;
     }
 }
+
 const getAdminTool  = async(req)=>{
     try {
         let getAdminTool = await sectionBuilder.find(req);
         return getAdminTool
+    } catch (error) {
+        let e = new ApiError(httpStatus.UNPROCESSABLE_ENTITY,error);
+        logger.error(`[Company Module Template Version] ${e}`)
+        throw e;
+    }
+}
+
+const getAdminToolInfo  = async(req)=>{
+    try {
+        const  section_id = new ObjectId(req.params.section_id);
+        const  adminTool_id = req.params.adminTool_id;
+        const adminTool = await sectionBuilder.findById(adminTool_id);
+        let sectionData = adminTool.sections;
+        let sectionContainer = [];
+        sectionData.map(v=>{
+            // console.log(v._id)
+        
+            if( JSON.stringify(section_id) === JSON.stringify(v._id)){
+                sectionContainer.push(v)
+            }
+        });
+        return sectionContainer
     } catch (error) {
         let e = new ApiError(httpStatus.UNPROCESSABLE_ENTITY,error);
         logger.error(`[Company Module Template Version] ${e}`)
@@ -45,10 +69,111 @@ const getAdminTool  = async(req)=>{
 //         throw e;
 //     }
 // }
+
+const patchAdminTool = async(req)  =>{
+    try {
+        // const company_id = req.params.company_id;
+        // const template_id = req.params.template_id;
+        // const version_id = req.params.version_id;
+        const  section_id = new ObjectId(req.params.section_id);
+        const  adminTool_id = req.params.adminTool_id;
+        const adminTool = await sectionBuilder.findById(adminTool_id);
+        let sectionData = adminTool.sections;
+        let sectionContainer = [];
+        sectionData.map(v=>{
+            // console.log(v._id)
+        
+            if( JSON.stringify(section_id) === JSON.stringify(v._id)){
+                v.address= (_.has(req.body,"address")) ? req.body.address : v.address;
+                v.sectionTitle= (_.has(req.body,"sectionTitle")) ? req.body.sectionTitle : v.sectionTitle;
+                v.order= (_.has(req.body,"order")) ? req.body.order : v.order;
+
+                if(_.has(req.body,"grayContent")){
+                    v.grayContent.dataType = (_.has(req.body.grayContent,"dataType")) ?req.body.grayContent.dataType :v.grayContent.dataType;
+                    v.grayContent.classes = (_.has(req.body.grayContent,"classes")) ?req.body.grayContent.classes :v.grayContent.classes;
+                    if(_.has(req.body.grayContent,"elements")){
+                        if(_.isEmpty(v.grayContent.elements)){
+                            v.grayContent.elements = req.body.grayContent.elements;
+                        }else{
+                            v.grayContent.elements.map(g=>{
+                                if(JSON.stringify(g._id) === JSON.stringify(req.body.grayContent.elements[0]._id)){
+                                    v.grayContent.elements = req.body.grayContent.elements[0];
+                                }else{
+                                    v.grayContent.elements.push(req.body.grayContent.elements[0]);
+                                }
+                            })
+                        }
+                        
+                    }
+                }
+
+
+                if(_.has(req.body,"headers")){
+                    v.headers.title.dataType = (_.has(req.body.headers,"dataType")) ? req.body.headers.dataType : v.headers.title.dataType;
+                    v.headers.title.description = (_.has(req.body.headers,"description")) ? req.body.headers.description : v.headers.title.description;
+                    v.headers.title.mainTitle = (_.has(req.body.headers,"mainTitle")) ?req.body.headers.mainTitle : v.headers.title.mainTitle;
+                    v.headers.title.subTitle = (_.has(req.body.headers,"subTitle")) ?req.body.headers.subTitle : v.headers.title.subTitle;
+
+                    // v.headers.title.quotes = (_.has(req.body.headers,"subTitle")) ?req.body.headers.subTitle : v.headers.title.subTitle;
+                    /**
+                     * Qoutes
+                     */
+                    if(_.has(req.body.headers,"quotes")){
+                        v.headers.title.quotes.dataType = (_.has(req.body.headers.quotes ,"dataType")) ? req.body.headers.quotes.dataType : v.headers.title.quotes.dataType;
+                        v.headers.title.quotes.position = (_.has(req.body.headers.quotes ,"position")) ? req.body.headers.quotes.position : v.headers.title.quotes.position;
+                        if((_.has(req.body.headers.quotes ,"elements"))){
+                            let dataElement = [];
+                            if(_.isEmpty(v.headers.title.quotes.elements)){
+                                v.headers.title.quotes.elements = req.body.headers.quotes.elements;
+                            }else{
+                                req.body.headers.quotes.elements.map(e=>{                                
+                                    dataElement.push({
+                                        dataType: (_.has(req.body.headers.quotes.elements),"dataType")?req.body.headers.quotes.elements.dataType:v.headers.title.quotes.elements.dataType,
+                                        qoute:  (_.has(req.body.headers.quotes.elements),"qoute")?req.body.headers.quotes.elements.qoute:v.headers.title.quotes.elements.qoute,
+                                    })
+                                })
+                            }
+                            
+                        }
+                    }
+                    /**
+                     * content
+                     */
+                           
+
+                }
+                
+
+                sectionContainer.push(v)
+            }
+        });
+
+        // console.log(sectionContainer.headers.title);
+
+
+        let qkey = {_id:adminTool_id};
+
+        // let update_id = new ObjectId(req.body.section_id)
+            qkey.sections = { $elemMatch: { _id: section_id} }
+  
+        let sectionEntry = await updateAdminTool({key:qkey,updateDoc:{$set:{'sections.$':sectionContainer[0]}}},{ returnDocument: 'after' })
+        return sectionEntry;
+
+
+        // Object.assign(adminTool,UpdateBody);
+        // await company.save();
+        // return company;
+
+    } catch (error) {
+        let e = new ApiError(httpStatus.UNPROCESSABLE_ENTITY,error);
+        logger.error(`[patch admin tool] ${e}`)
+        throw e;
+    }
+}
 const updateAdminTool  = async(req)  =>{
 
     try {
-        console.log(req.updateDoc)
+        console.log(req.key)
         const update = await  sectionBuilder.updateOne(req.key, req.updateDoc,{new:true});
         
         if(!update){
@@ -60,7 +185,7 @@ const updateAdminTool  = async(req)  =>{
 
     } catch (error) {
         let e = new ApiError(httpStatus.UNPROCESSABLE_ENTITY,error);
-        logger.error(`[Company Module Template Version] ${e}`)
+        logger.error(`[Update Admin Tool] ${e}`)
         throw e;
     }
 }
@@ -68,5 +193,7 @@ const updateAdminTool  = async(req)  =>{
 module.exports = {
     createAdminTool,
     getAdminTool,
-    updateAdminTool
+    updateAdminTool,
+    patchAdminTool,
+    getAdminToolInfo
 }
